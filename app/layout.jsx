0e5651bde21151fcf2495c0fefd731d7283e2331
James F <jamesaforeman@hotmail.com>
2023-05-08 17:08:39 +0100

    adding content

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import '@styles/globals.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+  title: "PromptMashin",
+  description: "Discover & Share Prompts for AI"
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
